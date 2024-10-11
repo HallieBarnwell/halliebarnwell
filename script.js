f60ccd63c7a55e17f35e20c49e26d18e24963c7e
HallieBarnwell <hbarnwel@samford.edu>
2024-10-11 12:47:59 -0500

    Added downloadData function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function downloadData() { 
+  event.preventDefault();
+  var startdate = documant.getElementById("startdate").value;
+  var enddate = documant.getElementById("enddate").value;
+  alert(startdate);
+  alert)(enddate);
+}
