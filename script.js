694f71213c8775db0e7ea95bac1d29ce8203a070
HallieBarnwell <hbarnwel@samford.edu>
2024-10-16 12:38:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function downloadData() {
   event.preventDefault();
   var startdate = documant.getElementById("startdate").value;
   var enddate = documant.getElementById("enddate").value;
-  console.log(startdate);
-  console.log(enddate);
+  console.log("startdate");
+  console.log("Ending date: " + enddate);
 }
