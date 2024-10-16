73e9a18e5e14cb8be7c3b534535945ee28860f03
HallieBarnwell <hbarnwel@samford.edu>
2024-10-16 12:50:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function downloadData() {
   event.preventDefault();
   var startdate = documant.getElementById("startdate").value;
   var enddate = documant.getElementById("enddate").value;
-  console.log("startdate");
+  console.log("Starting date: " + startdate);
   console.log("Ending date: " + enddate);
+  fetch("https://compute.samford.edu/zohauth/clients/data");
 }
